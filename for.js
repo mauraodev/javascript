216086276208845846d043f8d1109505f277edd4
Mauro Nunes <maurao.dev@gmail.com>
2018-11-24 01:06:36 -0200

    For and var

---
+++ for.js
@@ -0,0 +1,6 @@
+
+var Things = "ola mundo"
+
+for (var i = Things.length - 1; i >= 0; i--) {
+	console.log(Things[i])
+}
---
+++ var.js
@@ -0,0 +1,14 @@
+var x = 1;
+
+if (x === 1) {
+	var x = 2;
+}
+
+bla = 2;
+var bla;
+
+console.log(bla);
+
+var a, b = a = "A";
+
+console.log(b);
